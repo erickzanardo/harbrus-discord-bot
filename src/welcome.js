bb94400c510f514d3d89999012a9e6ed91837779
Erick Zanardo <erickzanardoo@gmail.com>
2019-09-16 18:15:45 -0300

    Fixing welcome message

--- src/welcome.js
+++ src/welcome.js
@@ -1,22 +1,20 @@
 const { getChannelByName, getRandomNumber } = require("./utils")
 
 const getWelcomeMessage = (guildMember, client) => {
-  const instruductionChannel = getChannelByName("introduction-yourself", client);
+  const introductionChannel = getChannelByName("introduce-yourself", client);
   const rulesChannel = getChannelByName("rules", client);
   const setYourRoleChannel = getChannelByName("set-your-role", client);
   const annoucementsChannel = getChannelByName("announcements", client);
 
   return`
-Welcome ${guildMember}, please introduce yourself on ${instruductionChannel}, take a look on ${rulesChannel}, pickup your roles on ${setYourRoleChannel} and keep an eye on ${annoucementsChannel}.
-
-We are looking for mentors and moderators, if you think you want to be one, send a DM to Harbrus.
+  Welcome ${guildMember}, I am Ghor Ranulf Parnell Borum Alum Belgarfind, General of the Dwarvish army, Dragons-Binder, Demons-Slayer, Bringer of Peace, and High-Wizard Professor here. Now, please introduce yourself on ${introductionChannel}, take a careful look on ${rulesChannel}, pickup your roles on ${setYourRoleChannel} and keep an eye on ${annoucementsChannel}. We have also a form to fill-in for people interested in teaching and studying (optional). <https://docs.google.com/forms/d/1gWWrB2Ectt5xvUqTLDiVk00btfB85T986hBaIes7Jbg/edit?usp=sharing>
 `
 }
 
 module.exports = {
   createWelcome: client => {
     client.on("guildMemberAdd", guildMember => {
-      const channel = getChannelByName("welcome-hall", client);
+      const channel = getChannelByName("welcome", client);
       channel.send(getWelcomeMessage(guildMember, client));
     });
   }
